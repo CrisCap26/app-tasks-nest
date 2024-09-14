e8835024368f40036936a096934957f26d5f815d
CrisH <cristopher.26hp@gmail.com>
2024-09-14 12:21:43 -0600

    Cambiando codigos de estado con el decorador HttpCode()

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller()
@@ -9,5 +9,17 @@ export class HelloController {
         response.status(200).json({
             msg: 'Hello world'
         })
+    }
+
+    @Get('notfound')
+    @HttpCode(404)
+    notFoundPage() {
+        return '404 Not Found';
+    }
+
+    @Get('error')
+    @HttpCode(500)
+    errorPage() {
+        return 'Error';
     }
 }
